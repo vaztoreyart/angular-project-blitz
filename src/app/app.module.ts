b6243eb6c74b74dc10fd0d479841dc047da57fe0
vaztoreyart <vaztoreyart@gmail.com>
2020-03-05 23:17:30 -0600

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProfileComponentComponent } from './profile-component/profile-component.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GridProfileComponent } from './card-profile/grid-profile/grid-profile.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProfileComponentComponent ],
+  declarations: [ AppComponent, HelloComponent, ProfileComponentComponent, GridProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/card-profile/grid-profile/grid-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-profile',
+  templateUrl: './grid-profile.component.html',
+  styleUrls: ['./grid-profile.component.css']
+})
+export class GridProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
